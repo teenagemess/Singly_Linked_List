349b97b510fe1fd83929c3099eaf8b77a62f6c70
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-11-09 17:04:58 +0700

    membuat class list dan menambahkan method addNode

--- Singly Linked List/Program.cs
+++ Singly Linked List/Program.cs
@@ -12,5 +12,49 @@ namespace Singly_Linked_List
         public string nama;
         public Node next;
     }
+    
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+        public void addNode()/*Method untuk menambahkan sebuah Node kedalam list*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nmasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
 
+            if (START == null || nim <= START.noMhs) /*Node ditambahkankan sebagai nodeBaru*/
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine(("\nNomer mahasiswa tidak diijinkan\n"));
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            /*menemukan lokasi node baru didalam list*/
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while((current != null) && (nim >= current.noMhs))
+            {
+                if(nim == current.noMhs)
+                {
+                    Console
+                }
+            }
+        }
+    }
 }
